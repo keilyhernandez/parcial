53b22331790d30d1f4d47c6ac875605672b015a0
keilyhernandez <keily.hernandez@catolica.edu.sv>
2021-03-12 22:15:56 -0600

    subiendo archivos

---
+++ parcial/parcial/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim numeros(6) As Integer
+        Dim suma As Integer
+
+        For i As Integer = 0 To numeros.Length - 1
+
+            Console.WriteLine("Inserta un numero: ")
+            numeros(i) = Console.ReadLine()
+            suma += numeros(i)
+
+        Next
+
+        Dim m As Double
+        m = suma / numeros.Length
+
+        Console.WriteLine("La media es: " & m)
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ parcial/parcial/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("parcial")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("parcial")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("5c8144cb-285e-4004-91a5-91c7dbaf2a5d")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+' mediante el carácter '*', como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
